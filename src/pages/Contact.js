11cf30429792249019b7adc7a46ef3b5d73692b2
Cindysaru <69647470+Cindysaru@users.noreply.github.com>
2020-08-17 09:28:37 +0300

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -44,7 +44,7 @@ import React from 'react'
         <ul className="contact-list">
           <li className="list-item"><i className="fa fa-map-marker fa-2x"><span className="contact-text place">Nairobi |KE </span></i></li>
           
-          <li className="list-item"><i className="fa fa-phone fa-2x"><span className="contact-text phone"><a href="tel" title="Give me a call">+254 712477675</a></span></i></li>
+          <li className="list-item"><i className="fa fa-phone fa-2x"><span className="contact-text phone"><a href="tel" title="Give me a call">+254 792487814</a></span></i></li>
           
           <li className="list-item"><i className="fa fa-envelope fa-2x"><span className="contact-text gmail"><a href="mailto:#" title="Send me an email">forexbiz@gmail.com</a></span></i></li>
           
